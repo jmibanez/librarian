3ee569eef91f60b4b7746234abf1c99d1aabae9c
JM Ibanez <jm@jmibanez.com>
2017-08-26 12:33:18 +0800

    Cleanup migration runner.
    Instead of blacklisting states, we whitelist only the states needed
    for migration -- our data source/connection pool, and our migration
    config.

--- src/com/jmibanez/librarian/migrations.clj
+++ src/com/jmibanez/librarian/migrations.clj
@@ -4,9 +4,7 @@
             [mount.core :as mount
              :refer [defstate]]
             [taoensso.timbre :as timbre]
-            [com.jmibanez.librarian
-             [config :as config]
-             [indexer :as idx]]))
+            [com.jmibanez.librarian.config :as config]))
 
 (timbre/refer-timbre)
 
@@ -18,13 +16,15 @@
   (repl/migrate *migration-config*))
 
 (defn migrate []
-  (mount/start-without #'idx/*indexer-pool*
-                       #'idx/*indexer-queue*
-                       #'idx/indexer-chan)
-  (do-migrate)
-  (mount/stop))
+  (mount/start #'config/*datasource*
+               #'*migration-config*)
+  (try
+    (do-migrate)
+    (finally
+      (mount/stop))))
 
 (defn rollback [& rest]
-  (mount/start)
+  (mount/start #'config/*datasource*
+               #'*migration-config*)
   (apply repl/rollback *migration-config* rest)
   (mount/stop))
