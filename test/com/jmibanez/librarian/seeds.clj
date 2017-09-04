6394f2030b40d8e2ae0b8f81aa249b7d55b82a0b
JM Ibanez <jm@jmibanez.com>
2017-09-05 06:52:33 +0800

    Seed test data using store.
    Seeding data for test fixtures using the store code can potentially be
    problematic. For instance, any bugs introduced in the store module can
    cause the seed data to be written or read incorrectly, and therefore
    will cause false positives (i.e. test failures). However, in return,
    for the downsides, we decouple our test data from the actual
    lower-level database representation, keeping all that knowledge
    abstracted inside the store, allowing us to rewire or refactor the
    store itself later on.

--- test/com/jmibanez/librarian/seeds.clj
+++ test/com/jmibanez/librarian/seeds.clj
@@ -1,11 +1,9 @@
 (ns com.jmibanez.librarian.seeds
-  (:require [hugsql.core :as hugsql]
-            [clojure.java.jdbc :as jdbc]
-            [com.jmibanez.librarian.config :as config]
-            [com.jmibanez.librarian.bootstrap :refer [do-bootstrap]]))
-
-(hugsql/def-db-fns "sql/base-seed.sql")
-
+  (:require [clojure.java.jdbc :as jdbc]
+            [com.jmibanez.librarian
+             [bootstrap :refer [do-bootstrap]]
+             [config :as config]
+             [store :as store]]))
 
 (def test-type #uuid "143246e3-c0b7-59e3-ae17-29b99ff0d5ac")
 (def test-other-type #uuid "13068b05-bc96-4ed6-9a5f-d49936da40da")
@@ -23,18 +21,62 @@
 
 (def test-doc-id-same-name #uuid "e649ce0d-801d-414c-bdc1-9605ff7090d1")
 
-(defn populate-db [conn]
-  (do-bootstrap)
-  (seed-test-document-headers! conn)
-  (seed-test-documents! conn))
+(def test-doc
+  {:id        test-doc-id
+   :name      test-doc-name
+   :type      test-type
+   :context   test-context
+   :state     :posted
+   :document  {:name "::string"}})
 
-(defn clear-db [conn]
-  (clear-test-documents! conn)
-  (clear-test-document-headers! conn))
+(def test-doc-same-name
+  {:id        test-doc-id-same-name
+   :name      test-doc-name
+   :type      test-other-type
+   :context   test-context
+   :state     :posted
+   :document  {:name "other"}})
+
+(def test-type-doc
+  {:id        test-type-id
+   :name      test-type-name
+   :type      store/schema-type
+   :context   test-context
+   :state     :posted
+   :document  {:definition {:id   "::string"
+                            :name "::string"
+                            :inner [{:name  "::string"
+                                     :value "::integer"}]}}})
+(def test-type-ref-doc
+  {:id        test-type-ref-id
+   :name      test-type-ref-name
+   :type      store/schema-type
+   :context   test-context
+   :state     :posted
+   :document  {:definition {:id       "::string"
+                            :name     "::string"
+                            :referred "TestDoc"}}})
+
+
+(def documents [test-doc test-doc-same-name test-type-doc test-type-ref-doc])
+
+(defn seed-test-documents! []
+  (let [tx (store/start-transaction! test-context)]
+    (try
+      (doseq [doc documents]
+        (->> doc
+             (store/map->Document)
+             (store/write-document! tx)))
+      (finally
+        (store/commit-transaction! tx)))))
+
+(defn populate-db []
+  (do-bootstrap)
+  (seed-test-documents!))
 
 (defn fixture [test]
   (jdbc/with-db-transaction [c config/*datasource*]
     (jdbc/db-set-rollback-only! c)
     (with-redefs [config/*datasource* c]
-      (populate-db c)
+      (populate-db)
       (test))))
